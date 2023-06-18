61924c00f858b78cfe573875586d7dbad61875bc
Andre Dias <andre.dias@alexandria.solar>
2023-06-18 13:14:53 -0300

    pages

---
+++ src/pages/about.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function About() {
+  return (
+    <>
+      <Head>
+        <title>WindFall</title>
+        <meta name="description" content="Web Site Windfall" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>about</h1>
+      </main>
+    </>
+  );
+}
---
+++ src/pages/contact.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Contact() {
+  return (
+    <>
+      <Head>
+        <title>WindFall</title>
+        <meta name="description" content="Web Site Windfall" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1>contato</h1>
+      </main>
+    </>
+  );
+}
