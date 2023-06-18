9b5cb57c54d827a050d96c85ef9be721bdef8e68
Andre Dias <andre.dias@alexandria.solar>
2023-06-18 14:06:50 -0300

    contato layout

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,5 +1,10 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
 import Head from "next/head";
+import { Inter } from "next/font/google";
+import styles from "@/styles/contact.module.css";
 
+const inter = Inter({ subsets: ["latin"] });
 export default function Contact() {
   return (
     <>
@@ -9,8 +14,55 @@ export default function Contact() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>contato</h1>
+      <main className={`${styles.main} ${inter.className}`}>
+        <Header />
+        <section className={styles["wrap-all"]}>
+          <form action="" className={styles["wrap-form"]}>
+            <div className={styles["wrap-title"]}>
+              <h1 className={styles["title"]}>Contate-nos pelo Whatsapp</h1>
+              <p className={styles["p"]}>
+                Nesta parte nos informe seus dados para facilitar o contato
+                adintando o assunto referente.
+              </p>
+            </div>
+            <div className={styles["wrap-input"]}>
+              <label className={styles["input-text"]} htmlFor="nome">
+                Nome
+              </label>
+              <input
+                className={styles["input-in"]}
+                id="nome"
+                type="text"
+                placeholder="Digite seu nome..."
+              />
+            </div>
+            <div className={styles["wrap-input"]}>
+              <label className={styles["input-text"]} htmlFor="email">
+                Email
+              </label>
+              <input
+                className={styles["input-in"]}
+                id="email"
+                type="email"
+                placeholder="Digite seu email..."
+              />
+            </div>
+
+            <div className={styles["wrap-input"]}>
+              <label className={styles["input-text"]} htmlFor="telefone">
+                Assunto
+              </label>
+              <select className={styles["input-in"]} id="telefone">
+                <option value="">Selecione um assunto...</option>
+                <option value="Matricula">Matricula</option>
+                <option value="FAQ">FAQ</option>
+                <option value="Outros">Outros</option>
+              </select>
+            </div>
+            <button className={styles["btn-send"]}>Enviar</button>
+          </form>
+        </section>
+        <Footer />
       </main>
     </>
   );
