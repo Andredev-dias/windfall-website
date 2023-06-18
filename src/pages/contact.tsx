8d22a1e874fb21c41881d9331e789dda7d37f8d7
Andre Dias <andre.dias@alexandria.solar>
2023-06-18 14:29:53 -0300

    contato funcionado

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
 import Head from "next/head";
@@ -5,7 +6,48 @@ import { Inter } from "next/font/google";
 import styles from "@/styles/contact.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
+
 export default function Contact() {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [assunto, setAssunto] = useState("");
+
+  const handleNomeChange = (event: any) => {
+    setNome(event.target.value);
+  };
+
+  const handleEmailChange = (event: any) => {
+    setEmail(event.target.value);
+  };
+
+  const handleAssuntoChange = (event: any) => {
+    setAssunto(event.target.value);
+  };
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+
+    if (nome && email) {
+      let mensagem = `Nome: ${nome} - `;
+      mensagem += `Email: ${email} - `;
+
+      if (assunto) {
+        mensagem += `Assunto: ${assunto} `;
+      }
+
+      const numeroTelefone = "554199994084"; // Substitua pelo número de telefone correto
+
+      window.open(
+        `https://api.whatsapp.com/send?phone=${numeroTelefone}&text=${encodeURIComponent(
+          mensagem
+        )}`,
+        "_blank"
+      );
+    } else {
+      alert("Por favor, preencha todos os campos do formulário.");
+    }
+  };
+
   return (
     <>
       <Head>
@@ -17,7 +59,11 @@ export default function Contact() {
       <main className={`${styles.main} ${inter.className}`}>
         <Header />
         <section className={styles["wrap-all"]}>
-          <form action="" className={styles["wrap-form"]}>
+          <form
+            onSubmit={handleSubmit}
+            className={styles["wrap-form"]}
+            target="_blank"
+          >
             <div className={styles["wrap-title"]}>
               <h1 className={styles["title"]}>Contate-nos pelo Whatsapp</h1>
               <p className={styles["p"]}>
@@ -34,6 +80,8 @@ export default function Contact() {
                 id="nome"
                 type="text"
                 placeholder="Digite seu nome..."
+                value={nome}
+                onChange={handleNomeChange}
               />
             </div>
             <div className={styles["wrap-input"]}>
@@ -45,21 +93,30 @@ export default function Contact() {
                 id="email"
                 type="email"
                 placeholder="Digite seu email..."
+                value={email}
+                onChange={handleEmailChange}
               />
             </div>
 
             <div className={styles["wrap-input"]}>
-              <label className={styles["input-text"]} htmlFor="telefone">
+              <label className={styles["input-text"]} htmlFor="assunto">
                 Assunto
               </label>
-              <select className={styles["input-in"]} id="telefone">
+              <select
+                className={styles["input-in"]}
+                id="assunto"
+                value={assunto}
+                onChange={handleAssuntoChange}
+              >
                 <option value="">Selecione um assunto...</option>
                 <option value="Matricula">Matricula</option>
                 <option value="FAQ">FAQ</option>
                 <option value="Outros">Outros</option>
               </select>
             </div>
-            <button className={styles["btn-send"]}>Enviar</button>
+            <button type="submit" className={styles["btn-send"]}>
+              Enviar
+            </button>
           </form>
         </section>
         <Footer />
